0e4cca06aaad2df93664b511cfe4ccc267d5fefc
Jared Ridabock <jaredridabock@gmail.com>
2021-03-25 14:28:14 -0700

    began TDD calculator

--- app.js
+++ app.js
@@ -1,5 +1,55 @@
 // import functions and grab DOM elements
+import { add, subtract, divide, toNumber, multiply } from './calculator.js';
 
-// initialize state
+const numberInputOne = document.getElementById('add-input-1');
+const numberInputTwo = document.getElementById('add-input-2');
+const addResult = document.getElementById('add-result');
+const addButton = document.getElementById('add-button'); 
 
-// set event listeners to update state and DOM
+addButton.addEventListener('click', () => {
+    
+    addResult.textContent = add(toNumber(numberInputOne), toNumber(numberInputTwo));
+});
+
+// subtract elements
+// subtract listener
+
+const subInputOne = document.getElementById('subtract-input-1');
+const subInputTwo = document.getElementById('subtract-input-2');
+const subButton = document.getElementById('subtract-button');
+const subResult = document.getElementById('subtract-result');
+
+subButton.addEventListener('click', () => {
+  
+    subResult.textContent = subtract(toNumber(subInputOne), toNumber(subInputTwo));
+
+    
+});
+
+//multiple elements
+// multiple listener
+const multInputOne = document.getElementById('mult-input-1');
+const multInputTwo = document.getElementById('mult-input-2');
+const multButton = document.getElementById('mult-button');
+const multResult = document.getElementById('mult-result');
+
+multButton.addEventListener('click', () => {
+    
+    multResult.textContent = multiply(toNumber(multInputOne), toNumber(multInputTwo));
+    
+});
+
+
+// divide elements
+// divide listener
+
+const divInputOne = document.getElementById('div-input-1');
+const divInputTwo = document.getElementById('div-input-2');
+const divButton = document.getElementById('div-button');
+const divResult = document.getElementById('div-result');
+
+divButton.addEventListener('click', () => {
+    
+    divResult.textContent = divide(toNumber(divInputOne), toNumber(divInputTwo));
+
+});
---
+++ test/calculator.test.js
@@ -0,0 +1,66 @@
+// IMPORT MODULES under test here:
+// import { example } from '../example.js';
+
+import { add, subtract, multiply, divide, toNumber } from '../calculator.js';
+
+const test = QUnit.test;
+
+
+
+test('time to test addition function', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 11;
+    const y = 2;
+    const expected = 13;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('test subtraction', (expect) => {
+    const x = 10;
+    const y = 8;
+    const expected = 2;
+
+    const actual = subtract(x, y);
+    expect.equal(actual, expected);
+
+});
+
+test('test multiplication', (expect) => {
+    const x = 5;
+    const y = 4;
+    const expected = 20;
+
+    const actual = multiply(x, y);
+    expect.equal(actual, expected); 
+
+});
+
+test('test division', (expect) => {
+    const x = 20;
+    const y = 4;
+    const expected = 5;
+
+    const actual = divide(x, y);
+    expect.equal(actual, expected); 
+
+});
+
+test('toNumber', (expect) => {
+    const expected = 7;
+    const input = document.createElement('input');
+    input.value = expected;
+
+    const actual = toNumber(input);
+
+    expect.equal(actual, expected);
+
+});
